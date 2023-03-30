677dedd0692407a4cacf451c9cf521d2ae48a464
sAmH365 <npower5377@naver.com>
2023-03-30 18:09:36 +0900

    유틸리티 객체와 날짜

--- src/main/kotlin/hello/thymeleaf/basic/BasicController.kt
+++ src/main/kotlin/hello/thymeleaf/basic/BasicController.kt
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import java.time.LocalDateTime
 
 @Controller
 @RequestMapping("/basic")
@@ -55,6 +56,12 @@ class BasicController {
         session.setAttribute("sessionData", "Hello Session")
 
         return "basic/basic-objects"
+    }
+
+    @GetMapping("/date")
+    fun date(model: Model): String {
+        model.addAttribute("localDateTime", LocalDateTime.now())
+        return "basic/date"
     }
 
     companion object {
